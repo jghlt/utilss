0595283767372596ec16333898c26701d57c3b6c
James Gavin Holt <jamesgavinholt1@gmail.com>
2018-04-17 14:47:55 -0400

    - some initial work on everything - - add some test files

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 #!/usr/bin/env node
-console.log('Hello, utilss!');
+
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const pjson = require('./package')
+const config = require('./config')
+const program = require('commander')
+
+program
+  .version(pjson.version)
+  .option('-i, --input <input>', 'input')
+  .option('-o, --output <output>', 'output')
+  .parse(process.argv)
+
+fs.readFile(program.input, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err)
+  }
+  const classes = extract(data)
+  const filtered = filter(classes)
+  console.log(classes)
+})
+
+function extract (file) {
+  let regex = /(?:class|className)=["']?([\d\w-_ @]+)+["']?/g
+  let array = []
+  let result
+
+  while (true) {
+    result = regex.exec(file)
+    if (!result) {
+      break
+    }
+    array = array.concat(result[1].split(' '))
+  }
+
+  if (array.length <= 1) {
+    return array
+  }
+
+  return array.filter(function (value, index) {
+    return array.indexOf(value) === index
+  })
+}
+
+//
+function filter (classes) {
+  let array = [];
+
+}
+
+//
+function create () {
+}
+
+//
+function save () {
+}
---
+++ test/public/assets/js/components/Masthead.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Masthead extends React.Component {
+
+  static propTypes = {
+  };
+
+  state = {
+  };
+
+  componentDidMount() {
+    console.log('Masthead: componentDidMount')
+  };
+
+  componentDidUpdate(){
+    console.log('Masthead: componentDidUpdate')
+  };
+
+  render(){
+    return (
+      <div id="Masthead">
+        <h1>Masthead</h1>
+      </div>
+    )
+  };
+};
+
+export default Masthead;
